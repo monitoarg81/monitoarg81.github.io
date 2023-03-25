0b802fb48dd04771df22b61fdc09026a24678b1c
monitoarg81 <126628375+monitoarg81@users.noreply.github.com>
2023-03-25 20:40:25 -0300

    Add files via upload

--- Javascript/funciones.js
+++ Javascript/funciones.js
@@ -1,32 +1,31 @@
 
 document.getElementById("bPersonales").addEventListener("click", function(){
-    document.getElementById("descripcion").style.visibility='visible';
-    document.getElementById("contactos").style.visibility='hidden';
-    document.getElementById("formacion").style.visibility='hidden';
-    document.getElementById("experiencia").style.visibility='hidden';
+    document.getElementById("cDatos").style.visibility='visible';
+    document.getElementById("cFormacion").style.visibility='hidden';
+    document.getElementById("cExp").style.visibility='hidden';
+    document.getElementById("cPerfil").style.color='rgb(87, 46, 12)';
 })
-document.getElementById("bContactos").addEventListener("click", function(){
-    document.getElementById("descripcion").style.visibility='hidden';
-    document.getElementById("formacion").style.visibility='hidden';
-    document.getElementById("experiencia").style.visibility='hidden';
-    document.getElementById("contactos").style.visibility='visible';
-})  
+document.getElementById("bPerfil").addEventListener("click", function(){
+    document.getElementById("cDatos").style.visibility='hidden';
+    document.getElementById("cFormacion").style.visibility='hidden';
+    document.getElementById("cExp").style.visibility='hidden';
+    document.getElementById("cPerfil").style.color='yellow';
+})
 
 document.getElementById("foto").addEventListener("click", function(){
-    document.getElementById("descripcion").style.visibility='visible';
-    document.getElementById("formacion").style.visibility='hidden';
-    document.getElementById("experiencia").style.visibility='hidden';
-    document.getElementById("contactos").style.visibility='hidden';
+    window.alert("Desarrollo Web by @Monitoarg81");
 })
+
 document.getElementById("bFormacion").addEventListener("click", function(){
-    document.getElementById("descripcion").style.visibility='hidden';
-    document.getElementById("contactos").style.visibility='hidden';
-    document.getElementById("experiencia").style.visibility='hidden';
-    document.getElementById("formacion").style.visibility='visible';
-})  
-document.getElementById("bExperiencia").addEventListener("click", function(){
-    document.getElementById("descripcion").style.visibility='hidden';
-    document.getElementById("formacion").style.visibility='hidden';
-    document.getElementById("contactos").style.visibility='hidden';
-    document.getElementById("experiencia").style.visibility='visible';
-})  
+    document.getElementById("cDatos").style.visibility='hidden';
+    document.getElementById("cFormacion").style.visibility='visible';
+    document.getElementById("cExp").style.visibility='hidden';
+    document.getElementById("cPerfil").style.color='rgb(87, 46, 12)';
+})
+
+document.getElementById("bExp").addEventListener("click", function(){
+    document.getElementById("cDatos").style.visibility='hidden';
+    document.getElementById("cFormacion").style.visibility='hidden';
+    document.getElementById("cExp").style.visibility='visible';
+    document.getElementById("cPerfil").style.color='rgb(87, 46, 12)';
+})
